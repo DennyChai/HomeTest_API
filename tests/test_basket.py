2c86e5642e769465aee7f0f218aa543a8e0a877d
DennyChai <a23914192@gmail.com>
2024-10-15 20:04:48 +0800

    Add check data after delete
    Add
    - Should not get basket data with deleted basket name

--- tests/test_basket.py
+++ tests/test_basket.py
@@ -52,11 +52,17 @@ class TestBasket:
     @pytest.mark.parametrize("name", test_data["basket_name"])
     def test_delete_entire_basket(self, basket_url, headers, name):
         basket_url = basket_url + name
-        response = requests.delete(basket_url, headers=headers)
-        assert "text/html" in response.headers["content-type"]
-        assert response.status_code == HTTPStatus.OK
-        response_text = response.text
+        response_delete_basket = requests.delete(basket_url, headers=headers)
+        assert "text/html" in response_delete_basket.headers["content-type"]
+        assert response_delete_basket.status_code == HTTPStatus.OK
+        response_text = response_delete_basket.text
         assert f"{name} was removed from your Pantry" in response_text
+
+        response_get_basket = requests.get(basket_url, headers=headers)
+        response_get_basket_text = response_get_basket.text
+        assert "text/html" in response_get_basket.headers["content-type"]
+        assert response_get_basket.status_code == HTTPStatus.BAD_REQUEST
+        assert f"Could not get basket: {name} does not exist" in response_get_basket_text
 
     @pytest.mark.parametrize("name", test_data["unknown_basket_name"])
     def test_delete_unregistered_basket(self, basket_url, headers, name):
